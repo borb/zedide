c4f9ab0af7bab2ac8377bd8a0840453d6daeb762
nine <nine@aphlor.org>
2020-07-26 19:01:13 +0100

    z80 syntax, webpack dev server

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,4 +1,11 @@
 import CodeMirror from 'codemirror'
 import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/z80/z80'
 
-const codeMirror = CodeMirror(document.body)
+const codeMirror = CodeMirror.fromTextArea(
+  document.getElementById('code-editor'),
+  {
+    lineNumbers: true,
+    mode: 'z80'
+  }
+)
