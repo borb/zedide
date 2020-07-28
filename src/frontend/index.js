48bfb213800560eb6ab1eaa1ece1e209026905d0
nine <nine@aphlor.org>
2020-07-28 22:18:07 +0100

    add code hints to the editor

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,11 +1,17 @@
 import CodeMirror from 'codemirror'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/z80/z80'
+import 'codemirror/addon/hint/show-hint'
+import 'codemirror/addon/hint/show-hint.css'
+import './hint/codemirror-z80'
 
 const codeMirror = CodeMirror.fromTextArea(
   document.getElementById('code-editor'),
   {
     lineNumbers: true,
+    extraKeys: {
+      "Ctrl-Space": "autocomplete"
+    },
     mode: 'z80'
   }
 )
