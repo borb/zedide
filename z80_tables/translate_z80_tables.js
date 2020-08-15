486c514c5af4573afcfc3cdbda074d0b94b8788a
nine <nine@aphlor.org>
2020-08-15 23:30:11 +0100

    handle all ld cases in base opcodes, and exx

--- z80_tables/translate_z80_tables.js
+++ z80_tables/translate_z80_tables.js
@@ -48,31 +48,90 @@ splitInput.forEach((line) => {
   switch (mnemonic) {
     case 'nop':
       // do nothing
+      // flags unaffected
       outputBuffer += `// ${mnemonic}\nthis.#opcodes[${opcode}] = () => {}\n`
       break
-    case 'ld':
-      // load instruction: determine if direct between registers, to/from memory, etc.
-      let parts = param.split(/,/)
 
-      if (parts[0].length == 1 && parts[1].length == 1) {
-        // simple byte register copy
-        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => { this.#registers.${parts[0]} = this.#registers.${parts[1]} }\n`
+    case 'ld': {
+      // load instruction: determine if direct between registers, to/from memory, etc.
+      // flags unaffected
+      const parts = param.split(/,/)
+
+      if ((parts[0].length == 1 && parts[1].length == 1)
+          || (parts[0].length == 2 && parts[1].length == 2)) {
+        // simple byte/word register copy
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => { this.#regops.${parts[0]}(this.#regops.${parts[1]}()) }\n`
         break
       }
 
-      if (parts[0].length == 2 && parts[1].length == 2) {
-        // simple word register copy (break into bytes)
-        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => {\n  this.#registers.${parts[0][0]} = this.#registers.${parts[1][0]}\n  this.#registers.${parts[0][1]} = this.#registers.${parts[1][1]}\n}\n`
+      if (parts[0].length == 1 && parts[1] == 'nn') {
+        // simple byte load
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => { this.#regops.${parts[0]}(this.#getPC()) }\n`
+        break
+      }
+
+      if (parts[0].length == 2 && parts[1] == 'nnnn') {
+        // simple word load
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => {\n  let [lo, hi] = [this.#getPC(), this.#getPC()]\n  this.#regops.${parts[0]}(this.#word(hi, lo)) }\n`
         break
       }
 
       if (parts[0] == '(nnnn)' && parts[1].length == 1) {
         // write byte register to memory
-        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => {\n  let [lo, hi] = [this.#getPC(), this.#getPC()]\n  this.#ram[this.#word(hi, lo)] = this.#registers.${parts[1]}\n}\n`
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => {\n  let [lo, hi] = [this.#getPC(), this.#getPC()]\n  this.#ram[this.#word(hi, lo)] = this.#regops.${parts[1]}()\n}\n`
+        break
+      }
+
+      if (parts[0] == '(nnnn)' && parts[1].length == 2) {
+        // write word register to memory
+        let [msb, lsb] = parts[1].split('')
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => {\n  let [lo, hi] = [this.#getPC(), this.#getPC()]\n  this.#ram[this.#word(hi, lo)] = this.#regops.${lsb}()\n  this.#ram[this.#addWord(this.#word(hi, lo), 1)] = this.#regops.${msb}()\n}\n`
+        break
+      }
+
+      if (parts[0].length == 1 && parts[1] == '(nnnn)') {
+        // load memory to byte register
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => {\n  let [lo, hi] = [this.#getPC(), this.#getPC()]\n  this.#regops.${parts[0]}(this.#ram[this.#word(hi, lo)])\n}\n`
+        break
+      }
+
+      if (parts[0].length == 2 && parts[1] == '(nnnn)') {
+        // load memory to word register
+        let [msb, lsb] = parts[1].split('')
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => {\n  let [lo, hi] = [this.#getPC(), this.#getPC()]\n  this.#regops.${lsb}(this.#ram[this.#word(hi, lo)])\n  this.#regops.${msb}(this.#ram[this.#addWord(this.#word(hi, lo), 1)])\n}\n`
+        break
+      }
+
+      if (parts[0].match(/\(..\)/) && parts[1] == 'nn') {
+        let register = parts[0].replace(/[()]/g, '')
+        // write byte to memory by register
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => { this.#ram[this.#regops.${register}()] = this.#getPC() }\n`
+        break
+      }
+
+      if (parts[0].match(/\(..\)/) && parts[1].length == 1) {
+        let register = parts[0].replace(/[()]/g, '')
+        // write 8-bit register to memory by register
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => { this.#ram[this.#regops.${register}()] = this.#regops.${parts[1]}() }\n`
+        break
+      }
+
+      if (parts[0].length == 1 && parts[1].match(/\(..\)/)) {
+        let register = parts[1].replace(/[()]/g, '')
+        // load 8-bit register from memory by register
+        outputBuffer += `// ${mnemonic} ${param}\nthis.#opcodes[${opcode}] = () => { this.#regops.${parts[0]}(this.#ram[this.#regops.${register}()]) }\n`
+        break
       }
 
       console.warn(`unhandled ld type: ${mnemonic} ${param}`)
       break
+    }
+
+    case 'exx':
+      // swap bc, de, hl with bc', de', hl'
+      outputBuffer += `// ${mnemonic}\nthis.#opcodes[${opcode}] = () => {\n  let [bc, de, hl] = [this.#regops.bc(), this.#regops.de(), this.#regops.hl()]\n  this.#regops.bc(this.#regops.bc2())\n  this.#regops.de(this.#regops.de2())\n  this.#regops.hl(this.#regops.hl2())\n  this.#regops.bc2(bc)\n  this.#regops.de2(de)\n  this.#regops.hl2(hl)\n}\n`
+      break
+
     default:
       console.warn(`unhandled mnemonic: ${mnemonic}`)
 
